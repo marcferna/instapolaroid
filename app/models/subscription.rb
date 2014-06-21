38514e606afa6e0aa6e51596346766f479f93cc8
Marc Fernandez <marc.fernandezg@gmail.com>
2014-06-21 16:34:45 -0700

    Added aspect media to tag subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,6 +4,7 @@ class Subscription
     Instagram.create_subscription(
       object:       'tag',
       callback_url: 'http://instapolaroid.herokuapp.com/callback',
+      aspect:        'media',
       object_id:     hashtag,
     )
   end

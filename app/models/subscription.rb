7920f910ed14756ebe1c45150cbad2b6fbb26d6e
Marc Fernandez <marc.fernandezg@gmail.com>
2014-06-21 16:26:57 -0700

    wip

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,8 +3,8 @@ class Subscription
   def self.create(hashtag)
     Instagram.create_subscription(
       object:       'tag',
-      callback_url: 'https://95xaxankq6b8.runscope.net/',
-      object_id:     hashtag
+      callback_url: 'http://instapolaroid.herokuapp.com/callback',
+      object_id:     hashtag,
     )
   end
 end

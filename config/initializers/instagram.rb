2fcc8066ddccf11c9a53c031d05896317c27ab37
Marc Fernandez <marc.fernandezg@gmail.com>
2014-06-22 18:47:37 -0700

    Changed environmental variables

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,5 +1,5 @@
 # setup instagram client with the CLIENT_ID and the CLIENT_SECRET
 Instagram.configure do |config|
-  config.client_id = ENV['CLIENT_ID']
-  config.client_secret = ENV['CLIENT_SECRET']
+  config.client_id = "ENV['INSTAGRAM_CLIENT_ID']"
+  config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
 end

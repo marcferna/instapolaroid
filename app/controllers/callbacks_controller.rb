5b9a90a838b36c221bd4b45f54523048bdfae61c
Marc Fernandez <marc.fernandezg@gmail.com>
2014-06-22 18:12:16 -0700

    skip verification of authenticity token

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,12 +1,16 @@
 class CallbacksController < ApplicationController
 
+  skip_before_filter :verify_authenticity_token
+
   # GET /callback
   def callback_verification
     render text: params["hub.challenge"]
   end
 
   # POST /callback
+  # Parameters: {"_json"=>[{"changed_aspect"=>"media", "object"=>"tag", "object_id"=>"marctest", "time"=>1403393505, "subscription_id"=>6459630, "data"=>{}}]
   def callback
-    # save image url and print
+    # go through the different tags and fetch the new images.
+    head :ok
   end
 end

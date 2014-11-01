915061d9f593fd8d2f42cda039641964ab5b27a4
Marc Fernandez <marc.fernandezg@gmail.com>
2014-11-01 14:44:20 -0700

    Add user relationships

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,21 +4,26 @@ class Photo < ActiveRecord::Base
 
   after_create :print
 
+  # Relationships
   belongs_to :event, counter_cache: true
+  delegate :user, to: :event
 
-  def self.create_with_parameters(photo, event_id)
-    Photo.create!(
-      event_id:           event_id,
-      instagram_photo_id: photo.id,
-      url:                photo.images.standard_resolution.url,
-      username:           photo.user.username,
-      parameters:         photo
-    )
+  class << self
+
+    def create_with_parameters(photo, event_id)
+      Photo.create!(
+        event_id:           event_id,
+        instagram_photo_id: photo.id,
+        url:                photo.images.standard_resolution.url,
+        username:           photo.user.username,
+        parameters:         photo
+      )
+    end
   end
 
-  protected
+private
 
-    def print
-      # system("lpr", "/Users/marc/Desktop/file.pdf") or raise "print failed"
-    end
+  def print
+    # system("lpr", "/Users/marc/Desktop/file.pdf") or raise "print failed"
+  end
 end

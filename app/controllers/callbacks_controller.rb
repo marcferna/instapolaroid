fc636e6d2fdcc0f6609b8e3085a491de9a6a51da
Marc Fernandez <marc.fernandezg@gmail.com>
2014-11-16 00:31:54 -0800

    Use Resque to process the Instagram callbacks.

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -10,13 +10,7 @@ class CallbacksController < ApplicationController
   # POST /callback
   # Parameters: {"_json"=>[{"changed_aspect"=>"media", "object"=>"tag", "object_id"=>"marctest", "time"=>1403393505, "subscription_id"=>6459630, "data"=>{}}]
   def callback
-    # go through the different tags and fetch the new images.
-    if params["_json"].present?
-      params["_json"].each do |event|
-        event = Event.find_by_hashtag(event["object_id"])
-        event.fetch_recent_photos if event.present?
-      end
-    end
+    Resque.enqueue(InstagramCallbackWorker, params)
     head :ok
   end
 end
--- app/models/subscription/instagram.rb
+++ app/models/subscription/instagram.rb
@@ -1,7 +1,7 @@
 class Subscription::Instagram
 
   def self.create(event_id, hashtag)
-    Resque.enqueue(InstagramWorker, event_id, hashtag)
+    Resque.enqueue(InstagramSubscriptionWorker, event_id, hashtag)
   end
 
 end
